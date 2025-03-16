aa8ba8ef09b0d3429863affe30b18472d834257e
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-16 09:19:43 +0100

    added APIkey

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const URL = "https://api.itgid.info";
-const APIKEY = "сюда_напишите_ваш_api_key";
+const APIKEY = "ccTqZO45ebuz2pPz";
 
 // никаких других изменений, добавлений
 // оптимизаций - в файл config не вносим!!!
